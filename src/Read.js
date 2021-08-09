3d9fe30e23bd6c1d5422a986cf06b7060ae6856f
Godwin Nwachukwu <nwgwin@gmail.com>
2021-08-09 19:11:18 +0000

    show shelfs in webpage

---
+++ src/Read.js
@@ -0,0 +1,44 @@
+import React from 'react'
+
+
+class Read extends React.Component {
+  state = {
+    read: []
+  }
+  
+  render(){
+    const books3 = this.props.read;
+    
+    return(
+      <div className="bookshelf-books">
+        <ol className="books-grid">
+           {books3.map((book)=>(
+             <li key={book.id}>
+               <div className="book">
+                 <div className="book-top">
+                   <div className="book-cover" style={{ width: 128, height: 193, backgroundImage:`url(${book.imageLinks})`}}></div>
+                   <div className="book-shelf-changer">
+                     <select>
+                       <option value="move" disabled>Move to...</option>
+                       <option value="currentlyReading">Currently Reading</option>
+                       <option value="wantToRead">Want to Read</option>
+                       <option value="read">Read</option>
+                       <option value="none">None</option>
+                     </select>
+                   </div>
+                 </div>
+                 <div className="book-title">{book.title}</div>
+                 <div className="book-authors">{book.authors}</div>
+                 <div>{book.subtitle}</div>
+                 <div>{book.shelf}</div>
+               </div>
+             </li>
+           ))}
+         </ol>
+       </div>
+    )
+  }
+}
+
+
+export default Read
